797cc38d398972f21526246371dc78cb425c2f7b
sunlei <121648522@qq.com>
2018-08-09 13:40:14 +0800

    添加项目文件。

---
+++ Asa.Common/ConvertHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asa.Common
+{
+    /// <summary>
+    /// 强制转换辅助类
+    /// </summary>
+    class ConvertHelper
+    {
+    }
+}
