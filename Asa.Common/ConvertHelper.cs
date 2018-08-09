81d1354c65dd5cae6f20955fe79e2f6b01c84fbc
sunlei <121648522@qq.com>
2018-08-09 13:52:27 +0800

    取第一个指定字符串之后的字符串

--- Asa.Common/ConvertHelper.cs
+++ Asa.Common/ConvertHelper.cs
@@ -9,7 +9,27 @@ namespace Asa.Common
     /// <summary>
     /// 强制转换辅助类 
     /// </summary>
-    class ConvertHelper
+   public static class ConvertHelper
     {
+        /// <summary>
+        /// 取第一个指定字符串之后的字符串
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name="obj"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string ObjToCharAfterString(this object obj,char args='_')
+        {
+            string result = string.Empty;
+            if (obj!=null)
+            {
+                string str = obj.ToString();
+                if (str.IndexOf(args)!=-1)
+                {
+                    result = str.Substring(str.LastIndexOf(args)).Trim(args);
+                }
+            }
+            return result;
+        }
     }
 }
